6e440b2c63121620aafe8217347877003539e57a
elwin2005 <151714685+elwin2005@users.noreply.github.com>
2024-05-27 23:50:22 -0700

    commit1

---
+++ condition.js
@@ -0,0 +1,49 @@
+// ( 5<10)?console.log("5 is lesser"):console.log("5 is greater");
+
+   
+
+// 1-11 Goood Morning
+// 12-6 Good Afernoon
+// 16-19 Good Evening
+// 19-24 Good Night
+
+
+// 
+
+// let fruits =["APPPLE","BANANA","GRAPES","PINAPPLE"];
+// for(let i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+// } 
+// // find the sum of the array
+// const number = [1,2,3,4,5];
+// var sum = 0;
+// for(let i =0 ; i<number.length;i++){
+//     sum = sum + number[i]
+
+// }
+// console.log(sum);
+
+
+// for(let i=1; i<=5 ;i++){
+//     for (let j =1 ; j<=5 ; j++){
+//         let p = ` ${i} * ${j} = ${i*j}` 
+//         console.log(p)
+//     }
+// }       
+    
+let p=[1,2.,3,4,5,6,7,8,9,10]
+
+for(let i=0;i<=p.length;i++){
+   if(p[i]%2==0){
+    console.log(`${p[i]}is even`)
+   }
+   else{
+   console.log(`${p[i]}is odd`)
+    
+   }
+}
+
+
+
+
+  
